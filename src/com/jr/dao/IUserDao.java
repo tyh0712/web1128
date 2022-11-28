109253ee112fbf1ee94fbbca7932d4a52f825a69
tangyh <tangyihan_0712@163.com>
2022-11-28 18:00:16 +0800

    first

---
+++ src/com/jr/dao/IUserDao.java
@@ -0,0 +1,10 @@
+package com.jr.dao;
+
+/**
+ * @Auther:唐一涵
+ * @Date:2022/11/28
+ * @Description: com.jr.dao
+ * @version: 1.0
+ */
+public interface IUserDao {
+}
---
+++ src/com/jr/entry/User.java
@@ -0,0 +1,10 @@
+package com.jr.entry;
+
+/**
+ * @Auther:唐一涵
+ * @Date:2022/11/28
+ * @Description: com.jr.entry
+ * @version: 1.0
+ */
+public class User {
+}
